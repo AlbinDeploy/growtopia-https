29519a9b6e412c1db66dd2ab8deb424783f68875
AlbinDeploy <albinpremam12@gmail.com>
2026-06-18 07:39:44 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,7 +2,7 @@ module.exports = {
     // your server name
     server_name: "Growtopia",
     // public ipv4
-    server_ip: "139.99.222.156",
+    server_ip: "168.144.36.2",
     // UDP port
     server_port: 55000,
     // login url dashboard
@@ -10,5 +10,5 @@ module.exports = {
     // for new server header
     type2: true,
     // meta
-    meta: "name=GrowPlus&ip=139.99.222.214port=55000&3rd=0",
+    meta: "name=GrowPlus&ip=168.144.36.2port=55000&3rd=0",
 }
