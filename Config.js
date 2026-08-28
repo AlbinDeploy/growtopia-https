1dc23f535eb63246a479c26774d9a1987794cc29
Albin <albinpremam12@gmail.com>
2026-08-29 06:15:34 +0700

    Update server configuration with new IP and login URL

--- Config.js
+++ Config.js
@@ -2,13 +2,13 @@ module.exports = {
     // your server name
     server_name: "Growtopia",
     // public ipv4
-    server_ip: "143.198.216.14",
+    server_ip: "152.42.164.233",
     // UDP port
     server_port: 55000,
     // login url dashboard
-    loginurl: "growtopia-login-backend-omega-seven.vercel.app",
+    loginurl: "login.albintsx.site",
     // for new server header
     type2: true,
     // meta
-    meta: "name=GrowPlus&ip=168.144.140.35port=55000&3rd=0",
+    meta: "name=GrowPlus&ip=152.42.164.233port=55000&3rd=0",
 }
